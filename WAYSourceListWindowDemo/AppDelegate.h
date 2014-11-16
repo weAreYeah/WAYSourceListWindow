8e586f0a3678db0fbab0354a00a46e176ced9f52
Raffael Hannemann <gitlab@raffael.me>
2014-11-16 12:13:06 +0100

    Initial Commit.

---
+++ WAYSourceListWindowDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WAYSourceListWindowDemo
+//
+//  Created by Raffael Hannemann on 16.11.14.
+//  Copyright (c) 2014 We Are Yeah!. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ WAYSourceListWindowDemo/AppDelegate.m
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.m
+//  WAYSourceListWindowDemo
+//
+//  Created by Raffael Hannemann on 16.11.14.
+//  Copyright (c) 2014 We Are Yeah!. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+	// Insert code here to initialize your application
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+	// Insert code here to tear down your application
+}
+
+@end
